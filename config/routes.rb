9dab593157d8095ff359c4ab88fb3934423ec3a2
vagrant <nknguyen@gmail.com>
2013-01-25 15:48:26 +0000

    finish demo

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Todo::Application.routes.draw do
+  resources :microposts
+
+
+  resources :microposts
+  resources :users
+
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
