c500b453a2c6cb0262e42280f03f88fbfe843b15
elsa <elsa@elsa.(none)>
2013-03-04 01:05:27 +0900

    update static pages

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -2,6 +2,19 @@ require 'spec_helper'
 
 describe "StaticPages" do
 
+  describe "Home Page" do
+    it "should have content in home page" do
+	visit '/static_pages/home'
+	page.should have_content('An application by PSU SWENG Team #4')
+    end
+  end
+  describe "Home Page" do
+    it "should have header tag in home page" do
+	visit '/static_pages/home'
+	page.should have_selector('h1', :text=> 'PSU To Do Projects')
+    end
+  end
+
   describe "Reports Page" do
     it "should have content in reports page" do
 	visit '/static_pages/report'
