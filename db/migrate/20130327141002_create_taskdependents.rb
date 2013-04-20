b289907773816bc729e8566cb3812c707e53d8f0
nam <mariners@gmail.com>
2013-04-21 03:24:49 +0900

    add col task dependent dep_id

--- db/migrate/20130327141002_create_taskdependents.rb
+++ db/migrate/20130327141002_create_taskdependents.rb
@@ -2,7 +2,7 @@ class CreateTaskdependents < ActiveRecord::Migration
   def change
     create_table :taskdependents do |t|
       t.integer :task_id
-      t.string :dep_id
+      t.integer :dep_id
       t.string :create
       t.string :destroy
 
