28f0187a2cfad350a91fc2a2055f474747dacc16
Ashwin Tambe <ashwintambe@gmail.com>
2013-04-21 19:23:10 -0400

    add some specs to model and controller

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,6 +1,35 @@
-FactoryGirl.define do
-  factory :user do
+require 'factory_girl'
+
+FactoryGirl.define do 
+	factory :user do
     username "joe@hotmail.com"
     password "abcdef"
   end
+end
+
+FactoryGirl.define :project do 
+	factory :project do
+	user_id "123"
+  	name "testproject1"
+ end
+end
+
+FactoryGirl.define :invalid_project do
+	factory :project do
+		user_id "999"
+		name nil
+	end		
+end
+
+FactoryGirl.define :task do
+	factory :task do
+	project_id "22"
+	description "task Test"
+ end
+
+FactoryGirl.define :invalid_task do
+	factory :task do
+		project_id "88"
+		description nil
+
 end
